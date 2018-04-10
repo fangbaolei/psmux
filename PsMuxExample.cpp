fd4c5371e7ceb3059d41f683a77870062c60d468
fang.baolei <181577354@qq.com>
2018-04-10 19:04:20 +0800

    #mod 002: 测试改用类的方式

--- PsMuxExample.cpp
+++ PsMuxExample.cpp
@@ -5,60 +5,15 @@
 
 #define BUF_LEN (1024*1024)
 
-struct PsMuxContext 
+class PsMuxContext
 {
-    PsMuxContext()
-    {
-        Idx = PsMux.AddStream(PSMUX_ST_VIDEO_H264);
-        pMuxBuf = new guint8[BUF_LEN];
-        pts = 0;
-        dts = 0;
-    }
-    ~PsMuxContext()
-    {
-        delete []pMuxBuf;
-    }
-    virtual void Process(guint8* buf, int len)
-    {
-        int MuxOutSize = 0;
-        int ret = PsMux.MuxH264SingleFrame(buf, len, pts, dts, Idx, pMuxBuf, &MuxOutSize, BUF_LEN);
-        
-        if (ret == 0 && MuxOutSize > 0){
-            OnPsFrameOut(pMuxBuf, MuxOutSize, pts, dts);
-        }
-        else if(ret == 3){
+public:
+    PsMuxContext();
+    ~PsMuxContext();
 
-        }
-        else{
-
-        }
-
-        unsigned char c = 0;
-        if (!isH264Or265Frame(buf, &c)){
-            return;
-        }
-
-        NAL_type Type = getH264NALtype(c);
-
-        if ((Type == NAL_IDR) || (Type == NAL_PFRAME)){
-            pts += 3600;
-            dts += 3600;
-        }
-    }
-
-    void PsProcessSaveFile(std::string DstName)
-    {
-        fpps = fopen(DstName.c_str(), "wb+");
-    }
-    
-    void OnPsFrameOut(guint8* buf, int len, gint64 pts, gint64 dts)
-       {
-           if (len > 0 && fpps)
-           {
-               fwrite(buf, len, 1, fpps);
-               fflush(fpps);
-           }
-       }
+    void Process(guint8* buf, int len);
+    void PsProcessSaveFile(std::string DstName);
+    void OnPsFrameOut(guint8* buf, int len, gint64 pts, gint64 dts);
 
 private:
     Gb28181PsMux PsMux;
@@ -67,8 +22,63 @@ private:
     gint64 pts;
     gint64 dts;
     FILE* fpps;
-
 };
+
+
+PsMuxContext::PsMuxContext()
+{
+    Idx = PsMux.AddStream(PSMUX_ST_VIDEO_H264);
+    pMuxBuf = new guint8[BUF_LEN];
+    pts = 0;
+    dts = 0;
+}
+
+PsMuxContext::~PsMuxContext()
+{
+    delete []pMuxBuf;
+}
+
+void PsMuxContext::Process(guint8* buf, int len)
+{
+    int MuxOutSize = 0;
+    int ret = PsMux.MuxH264SingleFrame(buf, len, pts, dts, Idx, pMuxBuf, &MuxOutSize, BUF_LEN);
+
+    if (ret == 0 && MuxOutSize > 0){
+        OnPsFrameOut(pMuxBuf, MuxOutSize, pts, dts);
+    }
+    else if(ret == 3){
+
+    }
+    else{
+
+    }
+
+    unsigned char c = 0;
+    if (!isH264Or265Frame(buf, &c)){
+        return;
+    }
+
+    NAL_type Type = getH264NALtype(c);
+
+    if ((Type == NAL_IDR) || (Type == NAL_PFRAME)){
+        pts += 3600;
+        dts += 3600;
+    }
+}
+
+void PsMuxContext::PsProcessSaveFile(std::string DstName)
+{
+    fpps = fopen(DstName.c_str(), "wb+");
+}
+
+void PsMuxContext::OnPsFrameOut(guint8* buf, int len, gint64 pts, gint64 dts)
+   {
+       if (len > 0 && fpps)
+       {
+           fwrite(buf, len, 1, fpps);
+           fflush(fpps);
+       }
+   }
 
 
 //遍历block拆分NALU,直到MaxSlice,不然一直遍历下去
@@ -133,7 +143,7 @@ int main(int argc, char* argv[])
     }
 
     guint8* fReadbuf = new guint8[BUF_LEN];
-    struct PsMuxContext psmuxcontext;
+    PsMuxContext psmuxcontext;
     psmuxcontext.PsProcessSaveFile("test.ps");
     while(1) {
         int fReadsz = fread(fReadbuf, 1, BUF_LEN, fp);
