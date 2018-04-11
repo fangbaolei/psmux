d8cb7cf4a0310552576d86faa848d6e5736826e8
fang.baolei <181577354@qq.com>
2018-04-11 17:22:26 +0800

    #mod 003: 测试改用类的方式

--- PsMuxExample.cpp
+++ PsMuxExample.cpp
@@ -11,10 +11,8 @@ public:
     PsMuxContext();
     ~PsMuxContext();
 
-    void Process(guint8* buf, int len);
-    void PsProcessSaveFile(std::string DstName);
-    void OnPsFrameOut(guint8* buf, int len, gint64 pts, gint64 dts);
-    int process_block(guint8* pBlock, int BlockLen, int MaxSlice);
+    void Process(guint8* buf, int len, FILE *fp);
+    int  process_block(guint8* pBlock, int BlockLen, int MaxSlice, FILE *fp);
 
 private:
     Gb28181PsMux PsMux;
@@ -30,8 +28,7 @@ PsMuxContext::PsMuxContext()
 {
     Idx = PsMux.AddStream(PSMUX_ST_VIDEO_H264);
     pMuxBuf = new guint8[BUF_LEN];
-    pts = 0;
-    dts = 0;
+
 }
 
 PsMuxContext::~PsMuxContext()
@@ -39,19 +36,15 @@ PsMuxContext::~PsMuxContext()
     delete []pMuxBuf;
 }
 
-void PsMuxContext::Process(guint8* buf, int len)
+void PsMuxContext::Process(guint8* buf, int len, FILE *fp)
 {
     int MuxOutSize = 0;
     int ret = PsMux.MuxH264SingleFrame(buf, len, pts, dts, Idx, pMuxBuf, &MuxOutSize, BUF_LEN);
 
     if (ret == 0 && MuxOutSize > 0){
-        OnPsFrameOut(pMuxBuf, MuxOutSize, pts, dts);
-    }
-    else if(ret == 3){
-
-    }
-    else{
-
+        printf("MuxOutSize %d\n", MuxOutSize);
+        fwrite(pMuxBuf, MuxOutSize, 1, fp);
+        fflush(fp);
     }
 
     unsigned char c = 0;
@@ -67,23 +60,8 @@ void PsMuxContext::Process(guint8* buf, int len)
     }
 }
 
-void PsMuxContext::PsProcessSaveFile(std::string DstName)
-{
-    fpps = fopen(DstName.c_str(), "wb+");
-}
-
-void PsMuxContext::OnPsFrameOut(guint8* buf, int len, gint64 pts, gint64 dts)
-   {
-       if (len > 0 && fpps)
-       {
-           fwrite(buf, len, 1, fpps);
-           fflush(fpps);
-       }
-   }
-
-
 //遍历block拆分NALU,直到MaxSlice,不然一直遍历下去
-int PsMuxContext::process_block(guint8* pBlock, int BlockLen, int MaxSlice)
+int PsMuxContext::process_block(guint8* pBlock, int BlockLen, int MaxSlice, FILE *fp)
 {
     static guint8* pStaticBuf = new guint8[BUF_LEN];
     static int StaticBufSize = 0;
@@ -106,7 +84,7 @@ int PsMuxContext::process_block(guint8* pBlock, int BlockLen, int MaxSlice)
     {
         if(isH264Or265Frame(pCurPos,NULL)){
             if (iSliceNum + 1 >= MaxSlice){//已经到达最大NALU个数,下面的不用找了把剩下的加上就是
-                this->Process(pCurPos, LastBlockLen);
+                this->Process(pCurPos, LastBlockLen, fp);
                 break;
             }
 
@@ -114,7 +92,7 @@ int PsMuxContext::process_block(guint8* pBlock, int BlockLen, int MaxSlice)
                 NaluStartPos = pCurPos;
             }
             else{
-                this->Process(NaluStartPos, pCurPos-NaluStartPos);
+                this->Process(NaluStartPos, pCurPos-NaluStartPos, fp);
                 iSliceNum++;
                 NaluStartPos = pCurPos;
             }
@@ -143,9 +121,15 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    FILE *fpps = fopen("test.ps", "wb+");
+    if (fpps == NULL)
+    {
+        printf("can't open file =\n");
+        return -1;
+    }
+
     guint8* fReadbuf = new guint8[BUF_LEN];
     PsMuxContext psmuxcontext;
-    psmuxcontext.PsProcessSaveFile("test.ps");
     while(1) {
         int fReadsz = fread(fReadbuf, 1, BUF_LEN, fp);
 
@@ -160,7 +144,7 @@ int main(int argc, char* argv[])
             }
         }
 
-        psmuxcontext.process_block(fReadbuf, fReadsz, 0xffff);
+        psmuxcontext.process_block(fReadbuf, fReadsz, 0xffff, fpps);
     }
 
     delete []fReadbuf;
